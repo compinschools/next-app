701946af08541f78d6fc53fad3752a992c9ad00b
Colin Smith <colinsmith@smithcolin.co.uk>
2024-10-21 10:55:05 +0100

    dogs

--- components/client/fact.js
+++ components/client/fact.js
@@ -2,31 +2,31 @@
 import {useState, useEffect} from "react";
 
 export default function Fact(){
-  const [fact, setFact] = useState(null);
+  const [facts, setFacts] = useState(null);
   
-  async function getFact(){ 
+  async function getFacts(){ 
     const data = await fetch("https://dogapi.dog/api/v2/facts?limit=5");
-    //const fact = await data.json();
+    
     const facts = await data.json();
-    console.log(facts);
-    setFact(facts.data);
+    console.log("facts",facts);
+    setFacts(facts.data);
   }
 
   useEffect(() => {
-    getFact();
+    getFacts();
   },[]);
 
   useEffect(() => {
-    console.log(fact);
-  },[fact]);
+    console.log(facts);
+  },[facts]);
       
 
     return ( <>
     {
-      fact?.map( (f) => <p className="pt-5" key={f.id}>{f.attributes.body}</p>)
+      facts?.map( (f) => <p className="pt-5" key={f.id}>{f.attributes.body}</p>)
     }
 
-    <button className="rounded-md bg-blue-600 p-3" onClick={getFact}>New Fact</button>
+    <button className="rounded-md bg-blue-600 p-3" onClick={getFacts}>New Fact</button>
     </>
   )
 }
